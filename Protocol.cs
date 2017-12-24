2575a5e68b9c5b84c387e661f781413947e518f5
Ahmed <charfeddine.ahmed@gmail.com>
2017-12-24 21:08:51 +0100

    initial commit

---
+++ Protocol.cs
@@ -0,0 +1,40 @@
+namespace ProtocolFramework
+{
+    public abstract class Protocol
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="incomingBytes"></param>
+        /// <param name="connectionToken"></param>
+        /// <returns></returns>
+        public abstract bool ReadBytes(Buffer incomingBytes, object connectionToken);
+        public abstract DecodeResult TryDecode(object connectionToken, out Buffer decodedBytes, out Buffer outputBytes);
+        public abstract EncodeResult Encode(object connectionToken, Buffer inputBuffer, out Buffer output);
+        public abstract object CreateConnectionToken();
+        public abstract bool IsNegociationEnded(object connectionToken);
+        public abstract void StartProtocolNegociation(object connectionToken, out Buffer outputBytes);
+
+        public abstract string Name
+        {
+            get;
+        }
+
+        public virtual string Version
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public virtual string Author
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+    }
+}
